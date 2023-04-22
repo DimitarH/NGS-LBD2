5d175ba5df14d3c23c25d75bd98aa3ee7e2ef0b0
Dimitar Hristovski <dimitar.hristovski@gmail.com>
2023-04-22 16:19:47 +0200

    Update seed_database2.sh

--- seed_database2.sh
+++ seed_database2.sh
@@ -8,11 +8,12 @@ echo Fetching data
 #    # mv 'semmed43cord19.dump?dl=1' /home/gbergant/ngslbd/neo4j/backup/semmed43cord19.dump
 #fi
 
-#if [ ! -f ./neo4j/backup/'semmed432202-2023-04-21-ftx-name.dump' ]; then
-# Get it from somewhere - maybe from my Google Drive.
+if [ ! -f ./neo4j/backup/semmed432202-2023-04-21-ftx-name.dump ]; then
+    mv ../semmed432202-2023-04-21-ftx-name.dump ./neo4j/backup
+#   Or get it from somewhere - maybe from Google Drive.
 #    wget https://www.dropbox.com/s/1fzi43umt6u49pm/semmed43cord19.dump?dl=1 -O /home/gbergant/ngslbd/neo4j/backup/semmed43cord19.dump
 #    # mv 'semmed43cord19.dump?dl=1' /home/gbergant/ngslbd/neo4j/backup/semmed43cord19.dump
-#fi
+fi
 
 if [ ! -f ./neo4j/import/gnomad.v2.1.1.lof_metrics.by_gene.txt ]; then
     wget https://storage.googleapis.com/gcp-public-data--gnomad/release/2.1.1/constraint/gnomad.v2.1.1.lof_metrics.by_gene.txt.bgz
@@ -26,14 +27,14 @@ echo Starting Neo4j
 
 docker-compose -f docker-compose-server2.yml up -d neo4jngs
 
-sleep 20
+sleep 30
 
 echo Restoring data
 #echo Restoring semmed43cord19
 echo Restoring semmed432202
 
 # docker exec -it neo4jngs bin/neo4j-admin load --from=/backup/'semmed43cord19.dump' --database=semmed43cord19 --force
-docker exec -it neo4jngs bin/neo4j-admin load --from=/backup/'semmed432202-2023-04-21-ftx-name.dump' --database=semmed432202 --force
+docker exec -it neo4jngs bin/neo4j-admin load --from=/backup/semmed432202-2023-04-21-ftx-name.dump --database=semmed432202 --force
 
 
 echo Configuring priviliges and creating databases
