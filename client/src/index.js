851f38a80b315325b151d71b4e4f62f5dc4851bf
Dimitar Hristovski <dimitar.hristovski@gmail.com>
2023-04-30 15:42:16 +0200

    Update index.js
    Changed the graphql server IP address.

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,8 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 console.log(`REACT_APP_GRAPHQL_URI: ${process.env.REACT_APP_GRAPHQL_URI}`)
 
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_URI || 'http://212.235.239.171:64001/graphql',
+uri: process.env.REACT_APP_GRAPHQL_URI || 'http://194.249.1.168:64001/graphql',
+//  uri: process.env.REACT_APP_GRAPHQL_URI || 'http://212.235.239.171:64001/graphql',
 //  uri: process.env.REACT_APP_GRAPHQL_URI || 'http://localhost:4001/graphql',
   cache: new InMemoryCache(),
 })
